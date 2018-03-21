c53ee571cb2fced8b1dc93a2bc20ccd0e18eec0a
Cesar Gomez <00.13.zkr@gmail.com>
2018-03-20 20:59:25 -0600

    init

---
+++ Views/Shared/footer.php
@@ -0,0 +1,62 @@
+<section id="clientes">
+    <div class="clientes-container">
+        <div class="container">
+            <h3 data-aos="flip-up">Algunas de nuestras marcas</h3>
+        </div>
+    </div>
+    <div id="clientes-carrousel" class="clientes-carrousel">
+        <img src="img/marcas/1.jpg" alt="">
+        <img src="img/marcas/2.png" alt="">
+        <img src="img/marcas/3.jpg" alt="">
+        <img src="img/marcas/4.png" alt="">
+        <img src="img/marcas/5.jpg" alt="">
+        <img src="img/marcas/6.png" alt="">
+        <img src="img/marcas/7.jpg" alt="">
+        <img src="img/marcas/8.png" alt="">
+        <img src="img/marcas/9.jpg" alt="">
+        <img src="img/marcas/10.png" alt="">
+        <img src="img/marcas/11.jpg" alt="">
+        <img src="img/marcas/12.png" alt="">
+        <img src="img/marcas/13.jpg" alt="">
+        <img src="img/marcas/14.jpg" alt="">
+        <img src="img/marcas/15.jpg" alt="">
+        <img src="img/marcas/16.jpg" alt="">
+        <img src="img/marcas/17.jpg" alt="">
+        <img src="img/marcas/18.jpg" alt="">
+        <img src="img/marcas/19.jpg" alt="">
+        <img src="img/marcas/20.jpg" alt="">
+        <img src="img/marcas/21.jpg" alt="">
+        <img src="img/marcas/22.jpg" alt="">
+        <img src="img/marcas/23.jpg" alt="">
+    </div>
+</section>
+<footer class="footer">
+    <div class="container-fluid">
+        <div class="row align-items-center">
+            <div class="col">
+                <div class="footer-social" data-aos="fade-up">
+                    <a href="#facebook"><i class="fa fa-facebook fa-2x"></i></a>
+                    <a href="#google-plus"><i class="fa fa-google-plus fa-2x"></i></a>
+                    <span>Síguenos</span>
+                </div>
+            </div>
+            <div class="col">
+                <div class="text-center" data-aos="fade-up">
+                    <a href="tel:8115247741">
+                        <i class="fa fa-phone fa-3x" aria-hidden="true"></i>
+                        <br>
+                        <span>+52 (81)18068310</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col">
+                <div class="footer-copy" data-aos="fade-up">
+                    <span>© Copyright 2017 GlobalSecurity</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<div id="copy">
+    <h5>Diseño Web <a href="http://cgomez.work">http://cgomez.work</a> <i class="fa fa-globe" aria-hidden="true"></i></h5>
+</div>
